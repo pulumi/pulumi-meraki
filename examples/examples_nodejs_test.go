c743e54a6d9cdea695435fe9913d179fd5368213
Ian Wahbe <ian@wahbe.com>
2024-05-20 16:45:13 -0700

    Add a test

---
+++ examples/examples_nodejs_test.go
@@ -0,0 +1,30 @@
+// Copyright 2016-2017, Pulumi Corporation.  All rights reserved.
+//go:build nodejs || all
+// +build nodejs all
+
+package examples
+
+import (
+	"path/filepath"
+	"testing"
+
+	"github.com/pulumi/pulumi/pkg/v3/testing/integration"
+)
+
+func TestAccRecordTs(t *testing.T) {
+	test := getJSBaseOptions(t).
+		With(integration.ProgramTestOptions{
+			Dir: filepath.Join(getCwd(t), "org-policy-groups", "ts"),
+		})
+
+	integration.ProgramTest(t, &test)
+}
+
+func getJSBaseOptions(t *testing.T) integration.ProgramTestOptions {
+	base := getBaseOptions(t)
+	baseJS := base.With(integration.ProgramTestOptions{
+		Dependencies: []string{"@pulumi/meraki"},
+	})
+
+	return baseJS
+}
