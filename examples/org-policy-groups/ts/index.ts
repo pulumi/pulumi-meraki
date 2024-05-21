3383655cdf93efd1048032a75800dc75905abd29
Ian Wahbe <ian@wahbe.com>
2024-05-20 17:07:04 -0700

    Ignore the "id" field unless it is computed & not optional (#21)
    * Ignore the "id" field unless it is computed & not optional
    
    Fixes #15
    
    * Add a test
    
    * Address comment

---
+++ examples/org-policy-groups/ts/index.ts
@@ -0,0 +1,16 @@
+import * as meraki from "@pulumi/meraki";
+
+export = async () => {
+    const orgs = await meraki.getOrganizations();
+
+    const adaptivePolicyGroups = new meraki.organizations.AdaptivePolicyGroups("grps", {
+        organizationId: orgs.items[0].id,
+        description: "test group by Pulumi",
+        name: "pulumi test",
+        sgt: 1005
+    });
+
+    return {
+        polGroups: adaptivePolicyGroups
+    }
+};
