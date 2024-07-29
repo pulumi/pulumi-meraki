ea58e829be80ad32df23805a2113eb3fe87d2332
phillip edwards <phillipedwards02@gmail.com>
2024-07-29 15:44:51 -0700

    removed some log files and moved package.json to top level for ts

--- examples/ts/organization-resources-ts/index.ts
+++ examples/ts/organization-resources-ts/index.ts
@@ -11,6 +11,39 @@ export = async () => {
         sgt: 1005
     });
 
+    const org = orgs.items[0];
+    const networks = await meraki.getNetworks({
+        organizationId: org.id
+    });
+
+    const network = networks.items[0];
+
+    // broken until https://github.com/pulumi/pulumi-meraki/issues/28 is resolved
+    new meraki.networks.AlertsSettings("alertsSettingsResource", {
+        networkId: network.id,
+        alerts: [{
+            alertDestinations: {
+                allAdmins: false,
+                emails: ["test@email.com"],
+                httpServerIds: ["aHR0cHM6Ly93d3cuZXhhbXBsZS5jb20vd2ViaG9va3M"],
+                snmp: false,
+            },
+            enabled: true,
+            filters: {
+                period: 0,
+                threshold: 0,
+                timeout: 0,
+            },
+            type: "gatewayDown",
+        }],
+        defaultDestinations: {
+            allAdmins: false,
+            emails: ["test@email.com"],
+            httpServerIds: ["aHR0cHM6Ly93d3cuZXhhbXBsZS5jb20vd2ViaG9va3M"],
+            snmp: false,
+        },
+    });
+
     return {
         polGroups: adaptivePolicyGroups
     }
